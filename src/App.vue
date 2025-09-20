6c76f3732f934bd812c68dc79e6285d0b8355e11
yudongxing <yuxs2022@163.com>
2025-09-20 10:04:10 +0800

    更新

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,38 @@
 <template>
   <div>
-    <h1>区块链</h1>
+    <div>
+      <h1>区块链</h1>
+    </div>
+    <el-tabs type="card" v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane v-for="item in tab" :key="item.name" :label="item.label" :name="item.name">{{ item.input }}</el-tab-pane>
+    </el-tabs>
   </div>
-  <el-tabs type="card" v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane
-      v-for="item in tab"
-      :key="item.name"
-      :label="item.label"
-      :name="item.name"
-    ></el-tab-pane>
-  </el-tabs>
+
 </template>
 
 <script>
+import user from './components/user.vue'
 export default {
+  components: { user },
   data() {
     return {
-      activeName: 'first',
-      tab:[{
+      activeName: '1',
+      tab: [{
         label: '用户管理',
-        name: 'first'
-      },{
+        name: '1',
+        input:'<user/>'
+      }, {
         label: '矿机管理',
-        name: 'second'
-      },{
-        label: '理财',
-        name: 'third'
-      },{
+        name: '2'
+
+      }, {
+        label: '理财管理',
+        name: '3'
+      }, {
         label: '设置',
-        name: 'fourth'
-      },
-      {
-        label: '帮助',
-        name: 'fifth'
+        name: '4'
       }
-    ]
+      ]
     };
   },
   methods: {
@@ -45,6 +43,4 @@ export default {
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/user.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>用户管理</h1>
+        <el-table :data="tableData">
+            <el-table-column prop="id" label="ID"></el-table-column>
+            <el-table-column prop="name" label="姓名"></el-table-column>
+            <el-table-column prop="age" label="年龄"></el-table-column>
+            <el-table-column prop="gender" label="性别"></el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'user',
+    data() {
+        return {
+        };
+    },
+    methods: {
+    },
+};
+</script>
+
+<style></style>
